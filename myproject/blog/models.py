6e9bab02fe0db822b965b37f68128db9e5131baa
fkilld <fkill.d.azadd@gmail.com>
2025-10-01 08:21:10 +0530

    1

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models  # import Django's model base classes and field typ
 from django.contrib.auth.models import User  # import the built-in User model provided by Django
 from django.urls import reverse  # import reverse to build URLs from named routes
 from django.db.models.signals import post_save  # import the post_save signal to run code after saving a model
+from django.dispatch import receiver
 
 
 class Category(models.Model):  # define a Category model to group blog posts
@@ -32,3 +33,74 @@ class Profile(models.model):
     def get_absolute_url(self):  
         return reverse('profile_detail', kwargs={'pk': self.pk})  
     
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):  # string representation of the category
+        return self.name 
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200) # url - friendly identifier for the posts must be  unique
+    content = models.TextField()
+    featured_image =  models.ImageField(upload_to='blog_images/', blank=True, null=True, help_text="Upload blog featured image")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') 
+    
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag,blank=True,related_name='posts')
+    created_at =  models.DateTimeField(auto_now_add=True)
+    updated_at =  models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    views = models.PositiveIntegerField(default=0)
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['slug','author']
+        
+    def get_absolute_url(self):  
+        return reverse('blog_detail', kwargs={'pk': self.pk})  
+        
+
+
+class Comment(models.Model):  
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')  
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')  
+    content = models.TextField(max_length=1000) 
+    created_at = models.DateTimeField(auto_now_add=True)  
+    is_approved = models.BooleanField(default=True)  
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')  
+
+    class Meta: 
+        ordering = ['created_at']  
+
+    def __str__(self):  
+        return f'Comment by {self.author.username} on {self.blog.title}' 
+    
+    
+class BlogLike(models.Model):
+    user  = models.ForeignKey(User,on_delete=models.CASCADE, related_name='liked_blog')
+    blog =  models.ForeignKey(Blog,on_delete=models.CASCADE,related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)  
+    
+
+    class Meta:
+        unique_together = ['slug','user']
+
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.blog.title}' 
+    
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
+@receiver(post_save,sender=User)
+def save_user_profile(sender,instance,created,**kwargs):
+    
+        instance.profile.save()
+        
+
+
